15f6e92dad6b9f5e331a968165658a31ac253a6e
Troy Mason <96906139+TroyCMason@users.noreply.github.com>
2023-05-24 14:14:34 -0400

    added JavaScript

--- function.js
+++ function.js
@@ -1,1 +1,9 @@
 // The best way to code is to build something meaningful from scratch
+
+function openMenu() {
+  document.body.classList += "menu__open"
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu__open")
+}
