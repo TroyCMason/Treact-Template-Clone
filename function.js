b382269ea4b89604af6b1b50f236563fa0649e5a
Troy Mason <troymason222@gmail.com>
2023-06-02 15:34:19 -0400

    new files added

--- function.js
+++ function.js
@@ -1,9 +1,10 @@
-// The best way to code is to build something meaningful from scratch
 
 function openMenu() {
-  document.body.classList += "menu__open"
+  document.body.classList += "menu__open";
 }
 
 function closeMenu() {
-  document.body.classList.remove("menu__open")
+  document.body.classList.remove("menu__open");
 }
+
+// The best way to code is to build something meaningful from scratch
