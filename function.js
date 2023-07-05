bdcce51cb2455d30824018d940a111ae96b35e53
Troy Mason <troymason222@gmail.com>
2023-07-05 15:23:20 -0400

    Added animation to burger menu

--- function.js
+++ function.js
@@ -1,10 +1,30 @@
+const menuButton = document.querySelector(".menu-button");
+
+let showMenu = false;
+menuButton.addEventListener("click", toggolmenu);
+
+//function
+function toggolmenu() {
+  if (!showMenu) {
+    menuButton.classList.add("close");
+    showMenu = true;
+  } else {
+    menuButton.classList.remove("close");
+    showMenu = false;
+  }
+}
+
 function openMenu() {
   document.body.classList += " menu--open"
 }
 
-function closeMenu() { 
+function closeMenu() {
   document.body.classList.remove('menu--open')
 }
+
+
+
+
 
 // The best way to code is to build something meaningful from scratch'
 
